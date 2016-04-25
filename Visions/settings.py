b70e62c85d1b23ba78bf9a8de3c59f8326d986c7
Ebubechim Okpo <ecotg@Ebubechims-MacBook-Pro.local>
2016-04-25 17:13:24 -0400

    Fix settings

--- Visions/settings.py
+++ Visions/settings.py
@@ -11,11 +11,11 @@ NEWSPIDER_MODULE = 'Visions.spiders'
 LOG_ENABLED = True
 LOG_ENCODING = 'utf-8'
 timestamp = datetime.now().strftime('%Y-%b-%d:%I-%M-%p')
-LOG_FILE = str(os.getcwd() + '/Visions' + '/LOGS/' + BOT_NAME + '-' + timestamp + '.log')
+LOG_FILE = str(os.getcwd() + '/Visions/LOGS/' + BOT_NAME + '-' + timestamp + '.log')
 
 # Export results, as a json feed, to file
-FEED_DIR = '/'.join(os.getcwd().split('/')[:-1]) + '/spiders/FEEDS'
-FEED_URI = 'file:///' + FEED_DIR + '/%(name)s' + '/' + timestamp + '.json'
+FEED_DIR = '/'.join(os.getcwd().split('/')[:-1]) + '/Visions/Visions/FEEDS/'
+FEED_URI = 'file:///' + FEED_DIR + timestamp + '.json'
 FEED_FORMAT = 'json'
 
 FEED_EXPORTERS = {
